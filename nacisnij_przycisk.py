1514ae83e33869e7a499e30555ba3b76deaa1f9c
P4vull0n <79141863+P4vull0n@users.noreply.github.com>
2021-02-16 13:57:13 +0100

    Add files via upload

---
+++ nacisnij_przycisk.py
@@ -0,0 +1,42 @@
+from tkinter import *
+import random
+
+t = Tk()
+t.title("Wybierz przycisk")
+t.geometry("300x350")
+
+def wstaw_przyciski():
+    ile_przyciskow = 8
+    global przyciski
+    przyciski = []
+    dobry = random.randint(0,ile_przyciskow-1)
+    for i in range (ile_przyciskow):
+        if i == dobry:
+            przyciski.append(Button(t,text = "kliknij mnie",command=trafiony))
+        else:
+            przyciski.append(Button(t,text = "kliknij mnie",command=pudlo))
+    for i in przyciski:
+        i.pack(fill=BOTH,expand=YES)
+    
+def trafiony():
+    for i in przyciski:
+        i.destroy()
+    global etykieta
+    etykieta = Label(t, text = "Trafiles dobry przycisk")
+    etykieta.pack(fill=BOTH,expand=YES)
+    t.after(5000,restart)
+
+def restart():
+    etykieta.destroy()
+    wstaw_przyciski()
+    
+def pudlo():
+    for i in przyciski:
+        i.destroy()
+    global etykieta
+    etykieta = Label(t, text = "Trafiles zly przycisk")
+    etykieta.pack(fill=BOTH,expand=YES)
+    t.after(5000,restart)
+
+wstaw_przyciski()
+                
